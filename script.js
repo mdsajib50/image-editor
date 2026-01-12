f84948c08a665386b88f1c58ada59ca51a2b3fbc
mdsajib50 <mdnurulalamsiddiki9@gmail.com>
2026-01-12 08:28:58 +0600

    applyFilter function done

--- script.js
+++ script.js
@@ -61,6 +61,14 @@ const filters = {
     }
 };
 
+const imageCanvas = document.getElementById("image-canvas");
+const imageInput = document.getElementById("input-image");
+
+const ctx = imageCanvas.getContext("2d");
+
+let file =null;
+let image =null;
+
 function createFilterElement(name, unit="%", value, min, max) {
     
 
@@ -81,6 +89,31 @@ function createFilterElement(name, unit="%", value, min, max) {
     container.appendChild(p);
     container.appendChild(input);
     container.appendChild(span);
+
+    input.addEventListener("input", function(event) {
+        filters[name].value = input.value;
+        applyFilters();
+        span.textContent = `${input.value}${unit}`;
+        // const filterName = event.target.dataset.filterName;
+        // const filterValue = event.target.value;
+        // span.textContent = `${filterValue}${unit}`;
+        // filters[filterName].value = filterValue;
+
+        // let filterString = "";
+        // Object.keys(filters).forEach(fname => {
+        //     const f = filters[fname];
+        //     let fValue = f.value;
+        //     if(fname === "hueRotate") {
+        //         filterString += `${fname}(${fValue}${f.unit}) `;
+        //     } else {
+        //         filterString += `${fname}(${fValue}${f.unit}) `;
+        //     }
+        // });
+
+        // ctx.filter = filterString.trim();
+        // ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+        // ctx.drawImage(image, 0, 0);
+    });
     return container;
 }
 
@@ -90,3 +123,33 @@ Object.keys(filters).forEach(filterName => {
     
     document.querySelector(".filter").appendChild(filterElement);
 });
+
+imageInput.addEventListener("change", function(event) {
+    const file = event.target.files[0];
+    const placeholder = document.querySelector(".placeholder");
+    placeholder.style.display = "none";
+
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.onload = function() {
+        image = img;
+        imageCanvas.width = img.width;
+        imageCanvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+    }
+
+})
+
+function applyFilters() {
+    ctx.filter =`brightness(${filters.brightness.value}${filters.brightness.unit})
+    contrast(${filters.contrast.value}${filters.contrast.unit})
+    saturate(${filters.saturation.value}${filters.saturation.unit})
+    grayscale(${filters.grayscale.value}${filters.grayscale.unit})
+    sepia(${filters.sepia.value}${filters.sepia.unit})
+    invert(${filters.invert.value}${filters.invert.unit})
+    hue-rotate(${filters.hueRotate.value}${filters.hueRotate.unit})
+    blur(${filters.blur.value}${filters.blur.unit})
+    opacity(${filters.opacity.value}${filters.opacity.unit})
+    `.trim();
+    ctx.drawImage(image, 0, 0);
+}
